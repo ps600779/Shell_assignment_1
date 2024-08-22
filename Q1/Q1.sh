28bd49838615e42bf31772afdd8ed8c1f7e383fb
GoLden1EaGle <priyanshu.sharma@opensenselabs.com>
2024-08-22 12:50:06 +0530

    first question

---
+++ Q1/Q1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+input_file="quotes.txt"
+
+grep -v '^\s*$' "$input_file" > quotes_empty.txt
+
+sort quotes_empty.txt | uniq > quotes_rdup.txt
+
+awk -F '-' '{personality[$2]++} END {for (p in personality) print p ": " personality[p]}' quotes_rdup.txt > quotes_byperson.txt
+
+grep -o '\bs[^a ]*' quotes_rdup.txt | sort | uniq
+
